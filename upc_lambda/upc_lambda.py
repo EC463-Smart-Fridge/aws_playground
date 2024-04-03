33853c80ddb7c614754d4696cc4ad8d6e030e7ec
Trevor Chan <trevchan@bu.edu>
2024-04-03 12:36:49 -0400

    Updated lambda handler to most up-to-date foodcentral calls

---
+++ upc_lambda/upc_lambda.py
@@ -0,0 +1,212 @@
+import json
+import requests
+import time
+import boto3
+from datetime import datetime, timedelta
+
+# Access the secrets / api keys stored in github secrets
+def findProductUsingUPC(searchTerm, api_key, lookupType):
+    
+    if lookupType == "smartScan": 
+        searchType = "Foundation,Survey,SRLegacy"
+    elif lookupType == "upc":
+        searchType = "Branded"
+    else:
+        searchType = "Foundation,Survey,SRLegacy,Branded"
+
+
+    # Define the API endpoint URL
+    url = f"https://api.nal.usda.gov/fdc/v1/foods/search?api_key={api_key}&query={searchTerm}&dataType={searchType}"
+
+    try:
+        # Send an HTTP GET request to the API endpoint
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            name = ""
+            category = ""
+            calories = 0
+            
+            try:
+                name = data["foods"][0]["description"].lstrip().capitalize()
+                category = data["foods"][0]["foodCategory"].lstrip().capitalize()
+
+                #Find block with kcal info
+                for index, block in enumerate(data["foods"][0]["foodNutrients"]):
+                    if block["nutrientId"] == 1008:
+                        calories = block["value"]
+                        break
+
+                return {"name": name, "category" : category, "calories" : calories}
+            
+            except:
+                try:
+                    url = f"https://api.nal.usda.gov/fdc/v1/foods/search?api_key={api_key}&query={searchTerm}" # widen search criteria
+                    response = requests.get(url)
+                    data = response.json()
+
+                    name = data["foods"][0]["description"].lstrip().capitalize()
+                    category = data["foods"][0]["foodCategory"].lstrip().capitalize()
+
+                    #Find block with kcal info
+                    for index, block in enumerate(data["foods"][0]["foodNutrients"]):
+                        if block["nutrientId"] == 1008:
+                            calories = block["value"]
+                            break
+
+                    return {"name": name, "category" : category, "calories" : calories}
+                except:
+                    return {"name": name, "category" : category, "calories" : calories}
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def getGenericNames(item):
+    # Define the endpoint
+    API_KEY = "531880dfcffc441f8773ac8ccbd4f2da"
+    api = "?apiKey=" + API_KEY
+    url = "https://api.spoonacular.com/food/detect/" + api
+
+    # Craft the POST request data
+    payload = {
+        'text': item,
+    }
+
+    try:
+        # Make the POST request
+        response = requests.post(url, data=payload)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Extract and work with the response data (if any)
+            data = response.json()
+
+            return data["annotations"][0]['annotation']
+
+        else:
+            # Handle errors
+            print("Error:", response.status_code, response.text)
+            return None
+    except Exception as e:
+        print("ERROR: Failed to get product name", e)
+        return None
+
+def predict_expiration(input_string):
+    # Load the predicted expiration times dataset
+    with open('expirationPredictions.json', 'r') as file:
+        expiration_data = json.load(file)
+
+    # Variables to keep track of the best match
+    best_match = None
+    best_score = 0
+
+    # Search for a product that best matches the user input
+    for product_id, product_info in expiration_data.items():
+        keywords = product_info.get('Keywords', [])
+
+        # Calculate match score for current product
+        score = sum(1 for keyword in keywords if keyword in input_string)
+
+        # Update best match if current product has higher score
+        if score > best_score:
+            best_match = product_info
+            best_score = score
+
+    # Output results
+    if best_match:
+        # Calculate predicted expiration date
+        expiration_time_days = best_match['Expiration_time_DAYS']
+        current_date = datetime.now().date()
+        expiration_date = current_date + timedelta(days=expiration_time_days)
+
+        # Convert to Unix timestamp format
+        expiration_datetime = datetime.combine(expiration_date, datetime.min.time())
+        expiration_timestamp = expiration_datetime.timestamp()
+        return int(expiration_timestamp)
+    else:
+        return None
+
+def insert_item(uid, upc, response_info, product_name):
+    
+    if (product_name):
+        item_name = product_name.title()
+        generic_name = product_name.title()
+    else:
+        item_name = response_info['name'].title()
+        generic_name = getGenericNames(response_info['name']).title()
+    
+
+    exp_pred = predict_expiration(generic_name.lower())
+
+    if (exp_pred):
+        exp_date = exp_pred
+    else:
+        exp_date = 0
+    
+    try:
+        db_client = boto3.client('dynamodb')
+        item_id = 'IT' + str(int(time.time() * 1000))
+        item = {
+            "pk": {'S': uid },
+            "sk": {'S': item_id },
+            "UPC":{'S': upc },
+            "name":{'S': item_name},
+            "quantity": {'N': '0' },
+            "exp_date": {'N': str(exp_date) },
+            "calories": {'S': str(response_info['calories']) },
+            "img_url": {'S': '' },
+            "category": {'S': response_info['category']},
+            "prod_name" : {'S': generic_name}
+        }
+        response = db_client.put_item(
+            TableName='fridgebase',
+            Item=item 
+        )
+        return {
+            "pk": uid,
+            "sk": item_id,
+            "UPC": upc,
+            "name": item_name,
+            "quantity": 0,
+            "exp_date": exp_date,
+            "calories": str(response_info['calories']),
+            "img_url": '',
+            "category": response_info['category'],
+            "prod_name": generic_name
+        }
+    except Exception as e:
+        print("ERROR: Failed to insert into DynamoDB:", e)
+        return {}
+
+def lambda_handler(event, context):
+    # Define the url to send the request to
+    upc = event['upc']
+    uid = event['uid']
+    product_name = event.get("name")
+    
+    api_key = "wn0aUcaBRdgzaIAXL9lzh69bEkskIAkPfolNO8RW"
+    
+    # Get the item information using the upc code
+    if (product_name):
+        searchType = "smartScan"
+    else:
+        searchType = "upc"
+
+    response_info = findProductUsingUPC(upc, api_key, searchType)
+
+    # If unable to get response information, return a 404 status code
+    if (response_info is None):
+        return {
+            'statusCode': 404,
+            'body': "ERROR: Item not found"
+        }
+        
+    response = insert_item(uid, upc, response_info, product_name)
+    return response
+
+print(lambda_handler({'upc' : "banana", 'uid' : 'UID1'}, None))
